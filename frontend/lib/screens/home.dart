c5eb6a6400923ea0d49e61f287589d19b06a5042
Subham Mittal <smittal@Subhams-Mac-mini.local>
2026-07-25 13:45:44 +0530

    feat(purchase_orders): add 5-module purchase orders workflow (Grey, Finish, Lace, Studio, Packing)

--- frontend/lib/screens/home.dart
+++ frontend/lib/screens/home.dart
@@ -10,6 +10,7 @@ import '../services/core/service_supabase.dart';
 import 'demo_screen.dart';
 import 'production/cutting/screen_cutting_landing.dart';
 import 'production/purchase_bills/screen_purchase_bills_landing.dart';
+import 'production/purchase_orders/screen_purchase_orders_landing.dart';
 import 'crm/screen_whatsapp_webview.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -495,6 +496,8 @@ class _HomeScreenState extends State<HomeScreen> with WindowListener {
       case 0:
       case 11:
         return const DemoScreen();
+      case 5:
+        return const ScreenPurchaseOrdersLanding();
       case 6:
         return const ScreenCuttingLanding();
       case 105:
